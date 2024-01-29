d54f1c04bfc3ad2fc2ec4b0eede3ea3060e17acf
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-01-28 23:25:45 -0300

    feat: component search form

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -8,6 +8,8 @@ export const HeaderContainer = styled.header`
 export const HeaderContent = styled.div`
   max-width: 1120px;
   margin: 0 auto;
+  padding: 0 1.5rem;
+
   display: flex;
   align-items: center;
   justify-content: space-between;
---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const SearchFormContainer = styled.form`
+  margin-top: 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+
+  input {
+    flex: 1;
+    padding: 1rem;
+    border: 0;
+    border-radius: 6px;
+    background: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']};
+
+    &::placeholder {
+      color: ${({ theme }) => theme['gray-300']};
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+
+    border: 0;
+    padding: 1rem 2rem;
+    background: transparent;
+    border: 1px solid ${({ theme }) => theme['green-300']};
+    color: ${({ theme }) => theme['green-300']};
+    font-weight: bold;
+    border-radius: 6px;
+
+    &:hover {
+      background: ${({ theme }) => theme['green-500']};
+      border-color: ${({ theme }) => theme['green-500']};
+      color: ${({ theme }) => theme.white};
+      transition:
+        background-color 0.2s,
+        border-color 0.2s,
+        color 0.2s;
+    }
+  }
+`
